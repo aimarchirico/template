040669d2545b46adf0553f223be0b659290da2aa
Aimár A. Chirico <aimar@chirico.no>
2026-06-13 12:06:41 +0200

    build(backend): add gradle wrapper and root project build configurations

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "template"
+
+include(":app")
