faf71352f5f5a8cd24b8ae0ab7746527c21bc82a
Aimár A. Chirico <aimar@chirico.no>
2026-06-30 12:49:50 +0000

    chore: revert api/client rename to backend/frontend

--- init/frontend.py
+++ init/frontend.py
@@ -2,7 +2,7 @@ import glob
 from text import lower
 from fs import replace_text
 
-def setup_client(default_mod, config_mod):
+def setup_frontend(default_mod, config_mod):
     def_name = default_mod["name"]
     cfg_name = config_mod["name"]
     def_pkg = default_mod["package"]
@@ -12,18 +12,18 @@ def setup_client(default_mod, config_mod):
     cfg_lower = lower(cfg_name)
 
     # Replacements for app.config.js
-    config_file = "client/apps/expo/app.config.js"
+    config_file = "frontend/apps/expo/app.config.js"
     replace_text(config_file, def_pkg, cfg_pkg)
     replace_text(config_file, def_name, cfg_name)
     replace_text(config_file, def_lower, cfg_lower)
 
     # Replace lower case name in all workspace configurations and workflows
     other_files = [
-        "client/package.json",
-        "client/apps/expo/package.json",
-        "client/apps/expo/tsconfig.json",
-        "client/apps/expo/eslint.config.mjs",
-        "client/packages/shared/api-client/package.json"
+        "frontend/package.json",
+        "frontend/apps/expo/package.json",
+        "frontend/apps/expo/tsconfig.json",
+        "frontend/apps/expo/eslint.config.mjs",
+        "frontend/packages/shared/api-client/package.json"
     ]
     for f in other_files:
         replace_text(f, def_lower, cfg_lower)
