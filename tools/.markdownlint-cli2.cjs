31677969a771e221cb2f47f1abf00c9fdf178335
Aimár A. Chirico <aimar@chirico.no>
2026-07-08 00:34:40 +0200

    chore: fix .markdownlint-cli2.cjs ignores

--- tools/.markdownlint-cli2.cjs
+++ tools/.markdownlint-cli2.cjs
@@ -3,5 +3,5 @@ const baseConfig = require('@aimarchirico/commons-tools/markdownlint');
 module.exports = {
   ...baseConfig,
   globs: ['../**/*.md'],
-  ignores: ['**/CHANGELOG.md']
+  ignores: ['../**/CHANGELOG.md']
 };
