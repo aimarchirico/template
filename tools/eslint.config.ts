62369771b65f56ed104a053b6ee682555e32dea0
Aimár A. Chirico <aimar@chirico.no>
2026-07-16 20:04:35 +0000

    refactor(eslint): remove wrapped dependencies from configs

--- tools/eslint.config.ts
+++ tools/eslint.config.ts
@@ -1,13 +1,7 @@
 import tsConfig from '@aimarchirico/commons-eslint';
-import jsonConfig from '@aimarchirico/commons-eslint/json';
-import ymlConfig from '@aimarchirico/commons-eslint/yaml';
-import tomlConfig from '@aimarchirico/commons-eslint/toml';
 
 export default [
   ...tsConfig,
-  ...jsonConfig,
-  ...ymlConfig,
-  ...tomlConfig,
   {
     ignores: [
       '../npm/packages/**/*',
