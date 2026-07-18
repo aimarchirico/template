0b7d4b366e2f6092cdf501f8b673fdde75b2de2b
Aimár A. Chirico <aimar@chirico.no>
2026-07-19 00:54:50 +0200

    chore: simplify eslint configuration by using baseConfig

--- frontend/apps/expo/eslint.config.ts
+++ frontend/apps/expo/eslint.config.ts
@@ -1,18 +1,2 @@
-import expoConfig from '@aimarchirico/commons-expo/eslint';
-
-export default [
-  ...expoConfig,
-  {
-    files: [
-      '*.js',
-      '*.mjs',
-      '*.ts',
-      '*.d.ts',
-      'scripts/**/*.js',
-      'functions/**/*.js',
-      'scripts/**/*.mjs',
-      'functions/**/*.mjs',
-    ],
-    rules: {'check-file/filename-naming-convention': 'off'},
-  },
-];
+import baseConfig from '@aimarchirico/commons-expo/eslint';
+export default baseConfig;
