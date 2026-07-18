323a347f9a1a06b4348da1977de9ec91f99820b9
Aimár A. Chirico <aimar@chirico.no>
2026-07-18 13:20:32 +0200

    chore: simplify tsconfig files by removing unnecessary include statements

--- tools/eslint.config.ts
+++ tools/eslint.config.ts
@@ -1,13 +1,11 @@
 import eslintPluginToml from 'eslint-plugin-toml';
 import eslintPluginYml from 'eslint-plugin-yml';
-
 import tsConfig from '@aimarchirico/commons-ts/eslint';
 
 export default [
   ...tsConfig,
   ...eslintPluginToml.configs['flat/recommended'],
   ...eslintPluginYml.configs['flat/recommended'],
-
   {
     ignores: [
       '**/frontend/apps/**',
