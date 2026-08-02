20db033f0144cdbf0230d81b1d27fdda604f5231
Aimár A. Chirico <aimar@chirico.no>
2026-08-02 21:33:42 +0200

    refactor: use standard defineConfig from eslint/config
    Replace custom defineEslintConfig function with the standard
    defineConfig imported from 'eslint/config', following the pattern
    used in commons-ts.

--- setup/eslint.config.ts
+++ setup/eslint.config.ts
@@ -1,11 +1,8 @@
+import {defineConfig} from 'eslint/config';
 import eslintBaseConfig from '@aimarchirico/commons-ts/eslint-core';
 
-function defineEslintConfig(config: typeof eslintBaseConfig) {
-  return config;
-}
-
 /** ESLint configuration. */
-export default defineEslintConfig([
+export default defineConfig([
   ...eslintBaseConfig,
   {
     ignores: ['manifest.resolved.json'],
