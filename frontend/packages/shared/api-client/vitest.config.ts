84abdc0f915c7d960c568252e55aef9089514b30
Aimár A. Chirico <aimar@chirico.no>
2026-08-03 13:16:38 +0200

    chore: update Vitest configuration to include coverage settings

--- frontend/packages/shared/api-client/vitest.config.ts
+++ frontend/packages/shared/api-client/vitest.config.ts
@@ -1,5 +1,13 @@
-import {defineConfig} from 'vitest/config';
+import {defineConfig, mergeConfig} from 'vitest/config';
 import config from '@aimarchirico/commons-ts/vitest-base';
 
 /** Vitest configuration. */
-export default defineConfig(config);
+export default defineConfig(
+  mergeConfig(config, {
+    test: {
+      coverage: {
+        exclude: ['src/services/generated/**'],
+      },
+    },
+  }),
+);
